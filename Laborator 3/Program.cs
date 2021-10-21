26ec457a22fccde059c4af9075be9700bab0c936
Madalina Florina Negrea <madalinanegrea40@gmail.com>
2021-10-21 19:35:40 +0300

    StringExtension

---
+++ Laborator 3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laborator_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "abc";
+            Console.WriteLine(s.ZoltanLast());
+        }
+    }
+}
---
+++ Laborator 3/StringExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laborator_3
+{
+    public static class StringExtension
+    {
+        public static char ZoltanLast(this string s)
+        {
+            if (!string.IsNullOrEmpty(s))
+            {
+                return s[s.Length - 1];
+            }
+            return (char)0;
+        }
+    }
+}
